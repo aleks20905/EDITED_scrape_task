e4be8521b66f20e1ab6826e4a4e6ec04f24a5df9
Bata Ti <aleks_20905@mail.bg>
2024-06-24 18:10:18 +0300

    pdp_call_detail

--- main.py
+++ main.py
@@ -10,13 +10,34 @@ driver = webdriver.Chrome(service=service)
 driver.get('https://www.marksandspencer.com/bg/easy-iron-geometric-print-shirt/p/P60639302.html')
 
 
-html_content = driver.page_source
+mainWraper = driver.find_elements(By.CLASS_NAME, "pdp_call_detail")
 
+elements = []
+for i in range(len(mainWraper)):
+    elements.append(mainWraper[i].text)
 
-with open('webpage.html', 'w', encoding='utf-8') as file:
-    file.write(html_content)
-print("Webpage saved")
+print(elements)
+
 
 driver.quit()
+
+# ['29 Reviews\n
+# BGN 115.00\n
+# COLOUR:\n
+# Blue Mix\n
+# SIZE\
+# nSize chart\n
+# Select Size\n
+# S-Regular Out of Stock\n
+# M-Regular\nL-Regular\n
+# XL-Regular\n
+# 2XL-Regular Out of Stock\n
+# 3XL-Regular\n
+# 4XL-Regular Out of Stock\n
+# 5XL-Regular Out of Stock\n
+# Notify me when back in stock\n
+# Add to bag\n
+# Save item for later\n
+# Free Express delivery over 200 BGN']
 
 
